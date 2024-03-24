a8ef6e9def14b45caf8232409714df834c64628a
dishant <dishu5570@gmail.com>
2024-03-24 12:32:33 +0530

    Update dependencies and refactor screenshot functionality

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import puppeteer from 'puppeteer-core';
+import captureWebsite from 'capture-website';
 import dotenv from 'dotenv';
-import locateChrome from 'locate-chrome';
 import sharp from 'sharp';
 
 dotenv.config();
@@ -27,96 +26,33 @@ app.get('/screenshot', async (req, res) => {
     return res.status(400).send('Missing URL');
   }
 
-  let browser;
-
-  try {
-    const executablePath = await new Promise(resolve => locateChrome((arg) => resolve(arg))) || '';
-    browser = await puppeteer.launch({
-      executablePath,
-      headless: true,
-      args: [
-        // Use the `--no-sandbox` flag in CI environment for Puppeteer to work correctly
-        '--no-sandbox',
-        //use of the `--disable-setuid-sandbox` flag is not recommended as it reduces the security of the Chrome instance
-        '--disable-setuid-sandbox',
-        //use of the `--disable-dev-shm-usage` flag to prevent the browser from using /dev/shm
-        '--disable-dev-shm-usage',
-        //use of the `--disable-accelerated-2d-canvas` flag to disable hardware acceleration
-        '--disable-gpu',
-        //use of the `--no-first-run` flag to prevent the browser from showing a first run dialog
-        '--no-first-run',
-        //use of the `--no-zygote` flag to prevent the browser from starting a zygote process for each child process
-        '--no-zygote',
-        //use of the `--single-process` flag to prevent the browser from starting multiple processes
-        '--single-process',
-        //use of the `--disable-extensions` flag to prevent the browser from loading extensions
-        '--disable-extensions',
-        //use of '--disable-feature=site-per-process' flag to disable site isolation
-        '--disable-features=site-per-process',
-
-      ],
-    });
-
-    const page = await browser.newPage();
-    await page.setViewport({
-      height: 1080,
-      width: 1920
-    });
-    await page.goto(url, {
-      waitUntil: ['load', 'domcontentloaded', 'networkidle0', 'networkidle2'],
-    });
-
-    if (parseInt(wait, 10) > 0) {
-      await sleep(
-        parseInt(wait, 10)
-      );
-    }
-    let attempt = 0;
-    let success = false;
-    let screenshot;
-
-    while (!success && attempt < 3) { // Attempt to capture the screenshot up to 3 times
-      try {
-        screenshot = await page.screenshot({ type: 'png' });
-        success = true; // If screenshot is successful, exit the loop
-      } catch (error) {
-        console.error(`Screenshot attempt ${attempt + 1} failed: ${error.message}`);
-        attempt++;
-        await sleep(1000); // Wait a bit before retrying
-      }
-    }
-
-    if (!success) {
-      throw new Error('Failed to capture screenshot after multiple attempts.');
-    }
-
-    const resizedScreenshot = await sharp(screenshot)
-      .resize(
-        parseInt(width, 10),
-        parseInt(height, 10),
-        {
-          fit: 'inside',
-          withoutEnlargement: true,
-        }
-      )
-      .toFormat(type, {
-        quality: parseInt(quality, 10),
-      })
-      .toBuffer();
-
-    res.type(`image/${type}`);
-
-    // Send the resized screenshot as the response
-    res.send(resizedScreenshot);
-
-  } catch (error) {
-    res.status(500).send(error.message);
-  } finally {
-    if (browser) {
-      await browser.close();
-    }
+  if (isNaN(width) || isNaN(height)) {
+    return res.status(400).send('Width and height must be numbers');
   }
 
+  if (isNaN(quality)) {
+    return res.status(400).send('Quality must be a number');
+  }
+
+  if (isNaN(wait)) {
+    return res.status(400).send('Wait must be a number');
+  }
+
+  const website = await captureWebsite.buffer(url, {
+    width: 1920,
+    height: 1080,
+    type,
+    quality: (type === 'webp' || type === 'jpeg') ? quality : undefined,
+    delay: parseFloat(wait),
+  });
+
+  const image = await sharp(website)
+    .resize(parseInt(width), parseInt(height))
+    .toBuffer();
+
+  res.type(`image/${type}`);
+
+  res.send(image);
 
 });
 
